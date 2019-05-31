9999aae9fd535864031f0e42f81ba4dbf0f8d54f
Edina Bolkeny <bolkeny.edina@gmail.com>
2019-05-31 20:27:17 +0100

    Add selected city temperature

--- src/interface.js
+++ src/interface.js
@@ -4,6 +4,7 @@ $(document).ready(function() {
    refreshTemperature();
    refreshPowerSavingMode();
 
+   // Register event handler functions
    $("#temperature-up").on('click', function() {
       thermostat.up();
       refreshTemperature();
@@ -23,6 +24,17 @@ $(document).ready(function() {
       thermostat.togglePowerSavingMode();
       refreshPowerSavingMode();
     });
+
+    $("#cities").on('change', function() {
+      var selectedCity = $(this).find("option:selected").text();
+      var selectedCityWeatherUrl = `http://api.openweathermap.org/data/2.5/weather?q=${selectedCity}&appid=a3d9eb01d4de82b9b8d0849ef604dbed&units=metric`
+
+      $.get(selectedCityWeatherUrl, function(data) {
+         $("#temp-outside").text(data.main.temp);
+       });
+    });
+
+    $("#cities").val("London").change();
 
     function refreshTemperature() {
       $("#temperature").text(thermostat.getTemperature() + " °C");
